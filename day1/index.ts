4101f2bcc90be6b64de92b51f20271500a6a9c20
Vladimir Gjorgievski <gorgievski.vladimir.l@gmail.com>
2021-12-01 11:05:48 +0100

    day 1

---
+++ day1/index.ts
@@ -0,0 +1,25 @@
+import { readFileSync } from "fs";
+
+const file = readFileSync("day1/input.txt").toString("utf-8");
+const input = file.split("\n");
+
+let deeper1 = 0, deeper2 = 0;
+
+for (let i = 0; i < input.length; i++) {
+    if (parseInt(input[i]) > parseInt(input[i - 1])) {
+        deeper1++;
+    }
+
+    const firstWindow =
+        parseInt(input[i]) + parseInt(input[i + 1]) + parseInt(input[i + 2]);
+    const secondWindow =
+        parseInt(input[i + 1]) +
+        parseInt(input[i + 2]) +
+        parseInt(input[i + 3]);
+
+    if (secondWindow > firstWindow) {
+        deeper2++;
+    }
+}
+
+console.log(`part1: ${deeper1}`, `part2: ${deeper2}`);
