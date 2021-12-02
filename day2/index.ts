9cc4c96be0eb735dc6f4fd44e8658248a1aab393
Vladimir Gjorgievski <gorgievski.vladimir.l@gmail.com>
2021-12-02 09:50:50 +0100

    day 2

---
+++ day2/index.ts
@@ -0,0 +1,55 @@
+import { readFileSync } from "fs";
+
+const file = readFileSync("day2/input.txt").toString("utf-8");
+const input = file.split("\n");
+
+//Part 1
+let x = 0, depth = 0;
+
+for (let i = 0; i < input.length; i++) {
+    const [command, value] = input[i].split(' ');
+
+    switch (command) {
+        case "forward":
+            x += parseInt(value);
+            break;
+        case "down":
+            depth += parseInt(value);
+            break;
+        case "up":
+            depth -= parseInt(value);
+            break;
+        default:
+            continue;
+    }
+
+}
+
+console.log(`part1: ${x * depth}`);
+
+
+//Part 2
+x = 0, depth = 0;
+let aim = 0;
+
+for (let i = 0; i < input.length; i++) {
+    const [command, value] = input[i].split(' ');
+
+    switch (command) {
+        case "forward":
+            x += parseInt(value);
+            depth += (parseInt(value) * aim);
+            break;
+        case "down":
+            aim += parseInt(value);
+            break;
+        case "up":
+            aim -= parseInt(value);
+            break;
+        default:
+            continue;
+    }
+
+}
+
+console.log(`part2: ${ x * depth }`);
